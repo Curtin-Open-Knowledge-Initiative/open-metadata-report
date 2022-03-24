23c923344074fccc03d58431734e259ebb4b3ec4
cameronneylon <cn@cameronneylon.net>
2022-03-24 13:29:08 +0000

    Minor cleanup in main.py

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ from pathlib import Path
 from precipy.main import render_file
 
 import data_process
-import graphs
 import data_parameters
-import graph_parameters
 
 # Run precipy
-render_file('config.json', [data_process, graphs], storages=[])
+render_file('config.json', [data_process], storages=[])
 
 # Make a dated archive of the output files
 output_store_path = Path('reports') / f'run_{data_parameters.TODAY_STR}'
