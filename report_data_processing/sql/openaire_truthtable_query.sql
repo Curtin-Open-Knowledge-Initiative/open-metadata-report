108bdb13fb5069b2d16b0432a14912d67531141c
Bianca Kramer <bianca.kramer@gmail.com>
2023-04-26 22:25:41 +0200

    Continue building OpenAIRE truthtable

--- report_data_processing/sql/openaire_truthtable_query.sql
+++ report_data_processing/sql/openaire_truthtable_query.sql
@@ -21,6 +21,30 @@ ARRAY(SELECT AS STRUCT subject.value, subject.scheme FROM unnest(subjects)) as s
 ARRAY(SELECT AS STRUCT GENERATE_UUID() as uuid, publicationdate, type, pid FROM unnest(instance)) as instance
 
 FROM `utrecht-university.TEMP.openaire_publication`
-)
+),
 
-SELECT * FROM INTERMEDIATE
+----- extract dois from nested column pids
+DOIS AS (
+
+ SELECT
+  id,
+  pid_check.value as doi,
+ FROM `utrecht-university.TEMP.openaire_publication_intermediate`,
+ UNNEST (pid) as pid_check
+ WHERE pid_check.scheme = 'doi'
+ ),
+
+ TABLE_EXPANDED AS (
+
+ SELECT
+
+ publications.id,
+ dois.doi,
+ publications.* EXCEPT (id)
+
+ FROM `utrecht-university.TEMP.openaire_publication_intermediate` as publications
+ LEFT JOIN DOIS as dois
+ ON publications.id = dois.id
+ )
+
+SELECT * FROM TABLE_EXPANDED
