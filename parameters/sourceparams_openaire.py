69f89b374c2a500521ca7b64781280ec3d9914e6
Bianca Kramer <bianca.kramer@gmail.com>
2023-04-28 15:32:38 +0200

    add source parameters file for openaire

---
+++ parameters/sourceparams_openaire.py
@@ -0,0 +1,38 @@
+"""
+Parameters for Source OpenAIRE
+
+To create a new source, make a copy of the sourceparams_template.py file and insert what is required
+
+NOTE: TODO for OpenAIRE, also tables "organization", "relation" and "project" are/will be used
+
+"""
+
+SOURCE_NAME = "openaire"
+SOURCE_PRINT_NAME = "OpenAIRE"
+SOURCE_GBQ_LOCATION = "academic-observatory.openaire"
+SOURCE_TABLE_NAME = "publication"
+SOURCE_TABLE_LOCATION = f"{SOURCE_GBQ_LOCATION}.{SOURCE_TABLE_NAME}"
+
+
+SOURCE_DATA_ELEMENTS = [
+    'authors',
+    'authors_id_source',
+    'authors_id_orcid',
+    'authors_string',
+    'authors_sequence',
+    'affiliations',
+    'affiliations_string',
+    'affiliations_id_ror',
+    'affiliations_id_source',
+    'affiliations_id_countrycode',
+    'abstract',
+    'citations',
+    'references',
+    'fields',
+    'venue',
+    'venue_string',
+    'venue_id_issn',
+    'venue_id_issnl',
+    'venue_id_source'
+]
+
