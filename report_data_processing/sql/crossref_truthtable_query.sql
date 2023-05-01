4a27f96b8c3ab003e2b7b22e93e089ed5786652e
Bianca Kramer <bianca.kramer@gmail.com>
2023-05-01 23:48:54 +0200

    add funders to crossref truthtable

--- report_data_processing/sql/crossref_truthtable_query.sql
+++ report_data_processing/sql/crossref_truthtable_query.sql
@@ -122,13 +122,12 @@ SELECT
     ARRAY_LENGTH(ISSN) as count_venue_id_issn,
 
     -- Funder
-    -- use ARRAY_LENGTH for funders as not all funder records have funder name
-    -- also include Funder DOIs as source ids (to enable comparison with Funder IDs in other systems)
     CASE
-        WHEN ARRAY_LENGTH(funder) > 0 THEN TRUE
+        WHEN (SELECT COUNT(1) FROM UNNEST(funder) AS funders WHERE funders.name is not null) > 0 THEN TRUE
         ELSE FALSE
-    END as has_funders,
-    ARRAY_LENGTH(funder) as count_funders,
+    END
+    as has_funders,
+    (SELECT COUNT(1) FROM UNNEST(funder) AS funders WHERE funders.name is not null) as count_funders,
     CASE
         WHEN (SELECT COUNT(1) FROM UNNEST(funder) AS funders WHERE funders.name is not null) > 0 THEN TRUE
         ELSE FALSE
@@ -138,13 +137,9 @@ SELECT
     CASE
         WHEN (SELECT COUNT(1) FROM UNNEST(funder) AS funders WHERE funders.DOI is not null) > 0 THEN TRUE
         ELSE FALSE
-    END as has_funders_id_source,
-    (SELECT COUNT(1) FROM UNNEST(funder) AS funders WHERE funders.DOI is not null) as count_funders_id_source,
-    CASE
-        WHEN (SELECT COUNT(1) FROM UNNEST(funder) AS funders WHERE funders.DOI is not null) > 0 THEN TRUE
-        ELSE FALSE
     END as has_funders_id_doi,
     (SELECT COUNT(1) FROM UNNEST(funder) AS funders WHERE funders.DOI is not null) as count_funders_id_doi,
+
 
 
 FROM table_cleaned
