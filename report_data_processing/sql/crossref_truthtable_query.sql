50fd408b498755973e3249ee7674637accb560ee
Bianca Kramer <bianca.kramer@gmail.com>
2023-05-04 13:31:04 +0200

    remove duplicate variables

--- report_data_processing/sql/crossref_truthtable_query.sql
+++ report_data_processing/sql/crossref_truthtable_query.sql
@@ -125,16 +125,10 @@ SELECT
     -- use ARRAY_LENGTH for funders as not all funder records have funder name
     -- also include Funder DOIs as source ids (to enable comparison with Funder IDs in other systems)
     CASE
-    WHEN ARRAY_LENGTH(funder) > 0 THEN TRUE
-    ELSE FALSE
-  END as has_funders,
-  ARRAY_LENGTH(funder) as count_funders,
-    CASE
-        WHEN (SELECT COUNT(1) FROM UNNEST(funder) AS funders WHERE funders.name is not null) > 0 THEN TRUE
+        WHEN ARRAY_LENGTH(funder) > 0 THEN TRUE
         ELSE FALSE
-    END
-    as has_funders,
-    (SELECT COUNT(1) FROM UNNEST(funder) AS funders WHERE funders.name is not null) as count_funders,
+    END as has_funders,
+    ARRAY_LENGTH(funder) as count_funders,
     CASE
         WHEN (SELECT COUNT(1) FROM UNNEST(funder) AS funders WHERE funders.name is not null) > 0 THEN TRUE
         ELSE FALSE
@@ -151,8 +145,6 @@ SELECT
         ELSE FALSE
     END as has_funders_id_doi,
     (SELECT COUNT(1) FROM UNNEST(funder) AS funders WHERE funders.DOI is not null) as count_funders_id_doi,
-
-
 
 
 FROM table_cleaned
