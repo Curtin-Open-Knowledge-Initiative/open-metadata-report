64061ef92a89cc0fc177eca613fbfc78b3a3319a
Bianca Kramer <bianca.kramer@gmail.com>
2023-08-10 08:13:16 +0200

    replace VALUE_ADD_META with original 3-way version

--- parameters/graph_parameters.py
+++ parameters/graph_parameters.py
@@ -158,20 +158,13 @@ def return_ys_dict(source_a,
 
 
 VALUE_ADD_META = {}
-for source_a in COMPARISON:
-    for source_b in COMPARISON:
-        if source_a == source_b: continue
+for source_a, source_b in itertools.permutations(SOURCES, 2):
 
-        VALUE_ADD_META.update(
-            {
-                source_a.SOURCE_NAME: {
-                    source_b.SOURCE_NAME: {
-                        'xs': value_add_meta_xs[source_a.SOURCE_NAME][source_b.SOURCE_NAME]['xs'],
-                        'ys': return_ys_dict(source_a, source_b)
-
-                    }
-                }
-            }
+        if not VALUE_ADD_META.get(source_a.SOURCE_NAME):
+            VALUE_ADD_META[source_a.SOURCE_NAME] = {}
+        VALUE_ADD_META[source_a.SOURCE_NAME][source_b.SOURCE_NAME] = dict(
+                        xs=value_add_meta_xs[source_a.SOURCE_NAME][source_b.SOURCE_NAME]['xs'],
+                        ys=return_ys_dict(source_a, source_b)
         )
 
 INTERNAL_COMPARISON_META = {
